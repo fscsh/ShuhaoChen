14e0a607cf26a9d59b2b7d366660e6debdca4634
Shuhao Chen <shuhao.chen@sjsu.edu>
2017-09-12 19:16:51 -0700

    deploy init

---
+++ js/contact.js
@@ -0,0 +1,10 @@
+$(".mat-input").click('focus', function() {
+    $(this).parent().addClass("is-active is-completed");
+});
+
+$(".mat-input").focusout(function() {
+    if ($(this).val() === "") {
+        $(this).parent().removeClass("is-completed");
+    }
+    $(this).parent().removeClass("is-active");
+});
---
+++ js/master.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('#games').hover(function(){
+        $('.games-dropdown').show();
+    },function(){
+        $('.games-dropdown').hide();
+    });
+
+});
